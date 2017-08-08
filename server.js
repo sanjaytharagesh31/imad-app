e823391d6dd60cadadadf3bde300e8bf31b6e65d
Sanjay Tharagesh <sanjay311999@gmail.com>
2017-08-08 23:21:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,6 +74,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter', function (req, res) {
+   counter = counter + 1;
+   res.send(counter.toString());
+});
+
 app.get('/:articleName', function (req, res){
     var articleName = req.params.articleName;
    res.send(createTemplate(articles[articleName]));
@@ -87,11 +93,6 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-var counter = 0;
-app.get('/counter', function (req, res) {
-   counter = counter + 1;
-   res.send(counter.toString());
-});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
