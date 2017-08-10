4a8aaf4ee715584aaabd59273f8a8fca6a01f9d7
Sanjay Tharagesh <sanjay311999@gmail.com>
2017-08-10 21:37:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,22 @@
-console.log('Loaded!');
-
-//change the text of main-text div
-var element = document.getElementById('main-text');
-element.innerHTML = 'i have changed';
-
-//move the madi when clicked
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight() {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function() {
-    var interval = setInterval(moveRight, 50);
+//counter code
+var button = document.getElementById('counter');
+button.onclick = function(){
+    
+    //create request object
+    var request = new XMLHttpRequest();
+    
+    //capture the request and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest,DONE){
+            if(request.readyState === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    
+    //make the request
+    request.open('GET', 'http://sanjay311999.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
