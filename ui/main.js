8e964dc678104fac083dc015bcd9d87c4863dbc6
Sanjay Tharagesh <sanjay311999@gmail.com>
2017-08-10 22:40:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -33,5 +33,21 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     //make a request to server and send name
+    var request = new XMLHttpRequest();
     
+    //capture the request and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest,DONE){
+            if(request.status === 200) {
+                //capture list of names and render it as list
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list ='';
+                for (var i=0; i<names.length; i++){
+                    list += '<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            }
+        }
 };
