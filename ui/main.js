16dcdf843c461e4200712ba1efae6acdbf577a92
Sanjay Tharagesh <sanjay311999@gmail.com>
2017-08-10 22:35:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,16 +9,21 @@ button.onclick = function(){
     request.onreadystatechange = function(){
         if(request.readyState === XMLHttpRequest,DONE){
             if(request.status === 200) {
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                counter = counter + 1;
-                span.innerHTML = counter.toString();
+                //capture list of names and render it as list
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list ='';
+                for (var i=0; i<names.length; i++){
+                    list += '<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
             }
         }
     };
     
     //make the request
-    request.open('GET', 'http://sanjay311999.imad.hasura-app.io/counter', true);
+    request.open('GET', 'http://sanjay311999.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
 };
 
@@ -28,12 +33,5 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     //make a request to server and send name
-    //capture list of names and render it as list
-    var names = ["name1", "name2", "name3", "name4"];
-    var list ='';
-    for (var i=0; i<names.length; i++){
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+    
 };
