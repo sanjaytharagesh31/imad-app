e3d3e90c61419b4272372aa3ab069fc823480d82
Sanjay Tharagesh <sanjay311999@gmail.com>
2017-08-16 22:55:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config = {
     database: "sanjay311999",
     host: "db.imad.hasura-app.io",
     port: "5432",
-    password: "process.env.DB_PASSWORD"
+    password: process.env.DB_PASSWORD
 };
 
 var articles = {
